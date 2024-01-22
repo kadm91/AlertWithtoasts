7e1d39296a888c2eb056019b0172a0ab4ccf1a74
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-22 10:21:42 -0700

    Initial Commit

---
+++ AlertWithtoasts/AlertWithtoastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertWithtoastsApp.swift
+//  AlertWithtoasts
+//
+//  Created by Kevin Martinez on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertWithtoastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
